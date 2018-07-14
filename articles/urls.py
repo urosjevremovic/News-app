744796b155cda042c53a98534fdbc46fc1bde2b8
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-15 00:31:23 +0200

    added logic for creating newspaper articles and adjusted permissions

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from articles.views import ArticleList, ArticleUpdate, ArticleDetail, ArticleDelete
+from articles.views import ArticleList, ArticleUpdate, ArticleDetail, ArticleDelete, ArticleCreate
 
 urlpatterns = [
     path('', ArticleList.as_view(), name='article_list'),
+    path('new/', ArticleCreate.as_view(), name='article_new'),
     path('<int:pk>/edit/', ArticleUpdate.as_view(), name='article_edit'),
     path('<int:pk>/', ArticleDetail.as_view(), name='article_detail'),
     path('<int:pk>/delete/', ArticleDelete.as_view(), name='article_delete'),
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.core.exceptions import PermissionDenied
 
 from articles.models import Article
 
@@ -11,40 +12,56 @@ class ArticleList(ListView):
     template_name = 'article_list.html'
 
 
-class ArticleDetail(DetailView):
+class ArticleDetail(DetailView, LoginRequiredMixin):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
+
+
+class ArticleCreate(CreateView, LoginRequiredMixin):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body', ]
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class ArticleUpdate(UpdateView, LoginRequiredMixin):
     model = Article
     fields = ['title', 'body']
     template_name = 'article_edit.html'
+    login_url = 'login'
 
     def dispatch(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         user = request.user
-        if user.is_staff:
+        if user.is_staff or user.is_superuser:
             Article.objects.get(pk=pk)
+            return super().dispatch(request, *args, **kwargs)
         try:
-            Article.objects.get(pk=pk, user=user)
+            Article.objects.get(pk=pk, author=user)
             return super().dispatch(request, *args, **kwargs)
         except Article.DoesNotExist:
-            return HttpResponseForbidden()
+            raise PermissionDenied
 
 
 class ArticleDelete(DeleteView, LoginRequiredMixin):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
     def dispatch(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         user = request.user
-        if user.is_staff:
+        if user.is_staff or user.is_superuser:
             Article.objects.get(pk=pk)
+            return super().dispatch(request, *args, **kwargs)
         try:
-            Article.objects.get(pk=pk, user=user)
+            Article.objects.get(pk=pk, author=user)
             return super().dispatch(request, *args, **kwargs)
         except Article.DoesNotExist:
-            return HttpResponseForbidden()
+            raise PermissionDenied
