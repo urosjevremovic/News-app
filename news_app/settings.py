8de6cdb2929a49fac313131785132400c2c34a10
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-15 19:59:36 +0200

    changed adding comment logic and set up api key in local env for send grid

--- news_app/settings.py
+++ news_app/settings.py
@@ -136,6 +136,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'Uros91'
-EMAIL_HOST_PASSWORD = 'radekoncar991'
+EMAIL_HOST_PASSWORD = os.environ.get('send_grip_password')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
