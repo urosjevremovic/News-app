ab07b1e1b5e86733ccff24faf89472da924ec12a
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-15 02:34:15 +0200

    fixed a bug in heroku conf

--- news_app/settings.py
+++ news_app/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic', 
     'django.contrib.staticfiles',
 
     'crispy_forms',
@@ -49,7 +48,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,8 +139,3 @@ EMAIL_HOST_USER = 'Uros91'
 EMAIL_HOST_PASSWORD = 'radekoncar991'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
